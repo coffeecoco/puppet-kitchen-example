2a70f15f0670ce4454687d6205afee9f1a752b8b
coffeecoco <coffeecoco@users.noreply.github.com>
2017-09-12 11:46:01 +1000

    serverspec

--- Gemfile
+++ Gemfile
@@ -9,3 +9,4 @@ gem "kitchen-puppet"
 gem "kitchen-vagrant"
 gem "librarian-puppet", :github => 'rodjek/librarian-puppet'
 gem "puppet", "~> 4.10.4"
+gem "serverspec"
---
+++ test/integration/default/serverspec/vagrant_spec.rb
@@ -0,0 +1,18 @@
+require 'serverspec'
+
+set :backend, :exec
+set :path, '$PATH:/sbin:/usr/sbin'
+
+describe 'vagrant' do
+  describe file('/etc/hosts') do
+    it {should be_file}
+    it {should be_mode '644'}
+    it {should be_owned_by 'root'}
+    it {should be_grouped_into 'root'}
+  end
+
+  describe file('/etc/puppet/puppet.conf') do
+    it {should be_file}
+    its(:content) { should match /server = puppet.example.com/ }
+  end
+end
